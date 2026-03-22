9f2f64565b3186856ae1d4ce2878f67cc67da1fe
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-22 22:21:54 +0200

    add files for new project

--- skillexchange/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <div>© <strong>ServiceExchange</strong> • 2025</div>
-            <div class="muted">Помощ • Политика за поверителност • Контакти</div>
-        </footer>
-    );
-}
