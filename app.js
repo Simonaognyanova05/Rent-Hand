64fccd0abc9077e7456d71fa44764b03c625bfe6
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-03-26 09:07:21 +0200

    add logout

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.put('/catalog/edit/:id', isAuth, serviceController.service_edit_one);
 app.get('/about', about);
 app.get('/contact', contact);
 app.get('/login', login.login_get);
+app.get('/logout', login.logout);
 app.post('/login', login.login_post);
 app.get('/register', register.register_get);
 app.post('/register', register.register_post);
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -46,4 +46,14 @@ const login_post = async (req, res) => {
     }
 };
 
-module.exports = { login_get, login_post };
+const logout = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return console.log(err);
+        };
+
+        res.redirect('/');
+    })
+}
+
+module.exports = { login_get, login_post, logout };
