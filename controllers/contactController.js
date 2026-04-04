e9fbd152c0ff09768893402f53821a2c374a1a05
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-04-04 14:10:44 +0300

    add sanding message from contacts form

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,5 +1,21 @@
-const contact = (req, res) => {
+const Contact = require('../models/contact');
+
+const contact_get = (req, res) => {
     res.render('contact', { title: 'Contact page' });
 };
 
-module.exports = contact;
+const contact_post = async (req, res) => {
+    const messageData = req.body;
+
+    try {
+        const contact = new Contact(messageData);
+
+        await contact.save();
+        res.redirect('/');
+    } catch (e) {
+        console.log(e);
+        res.redirect('/contact');
+    }
+}
+
+module.exports = { contact_get, contact_post };
